9856c25b6808a3b91442dfc2911c7c35d4bf39de
regym48 <83573516+regym48@users.noreply.github.com>
2021-06-30 17:05:31 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,13 @@
+var x = "Semangat...";
+console.log(x.repeat(5))
+console.log("-----------------------------------------------")
+var y = "*****";
+console.log(y)
+console.log(y.repeat(4))
+console.log(y.repeat(8))
+console.log(y.repeat(14))
+console.log(y.repeat(20))
+console.log(y.repeat(14))
+console.log(y.repeat(8))
+console.log(y.repeat(4))
+console.log(y)
